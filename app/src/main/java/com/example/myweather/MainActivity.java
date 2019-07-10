bb314d777ec2d88a8ab63e6d96e4e2ad878050d0
dkowsikpai <dkowsikpai@protonmail.com>
2019-07-10 12:07:06 +0530

    0.2v

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.myweather;
 
+import android.content.Context;
+import android.hardware.input.InputManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -48,6 +59,61 @@ public class MainActivity extends AppCompatActivity {
             }
             return result;
         }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+
+                String message = "";
+
+                JSONObject jsonObject = new JSONObject(result);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < arr.length(); i++) {
+
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+
+                    if (main != "" && description != "") {
+
+                        message += main + ": " + description + "\r\n";
+
+                    }
+
+                }
+
+                if (message != "") {
+
+                    textView.setText(message);
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+
+                }
+
+
+            } catch (JSONException e) {
+
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+
+            }
+
+
+
+        }
     }
 
     @Override
@@ -56,18 +122,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editText);
         textView = findViewById(R.id.weather);
+
     }
 
     public void submit(View view) {
-
-        DownloadTask task = new DownloadTask();
-        try {
-            String result = task.execute("http://api.openweathermap.org/data/2.5/weather?appid=964414f2657438c0646be66181a672d9&q=" + editText.getText()).get();
-            textView.setText(result);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (editText.getText().length() > 0) {
+            InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            mgr.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+            DownloadTask task = new DownloadTask();
+            try {
+                String encodedCityName = URLEncoder.encode(editText.getText().toString(), "UTF-8");
+                task.execute("http://api.openweathermap.org/data/2.5/weather?appid=964414f2657438c0646be66181a672d9&q=" + encodedCityName);
+                textView.setText("Loading...");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else  {
+            Toast.makeText(getApplicationContext(), "Enter the location", Toast.LENGTH_LONG).show();
         }
 
     }
